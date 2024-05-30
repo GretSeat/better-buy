10e2a903f5b351500923ae98a6e00e16211e2b97
Garrett Stewart <gretseatdev@gmail.com>
2024-05-30 15:03:52 -0400

    Add Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
 
 const App = () => {
-  return <h1>Welcome to Better Buy!</h1>;
+  return (
+    <>
+      <Header />
+      <main classname="py-3">
+        <Container>
+          <h1>Welcome to Better Buy!</h1>
+        </Container>
+      </main>
+    </>
+  );
 };
 
 export default App;
