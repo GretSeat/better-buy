bfa3c07f06b05df1ad22c3caf8e98bd175855b03
Garrett Stewart <gretseatdev@gmail.com>
2024-05-31 11:10:48 -0400

    Add Product Page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,12 +10,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Home from "./pages/Home";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<Home />} />
+      <Route index={true} path="/" element={<HomePage />} />
+      <Route path="/product/:id" element={<ProductPage />} />
     </Route>
   )
 );
